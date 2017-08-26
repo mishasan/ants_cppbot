e02b5f766294cfc2e79ee4eb780fd41bf6ecb9d6
mishasan <micha_heinrich@gmx.net>
2017-08-27 07:04:05 +1200

    added DeBug: a way to hold the bot to wait for attaching the Debugger to the process.

--- Bug.h
+++ Bug.h
@@ -61,4 +61,54 @@ inline Bug& operator<<(Bug &bug, const T &t)
     return bug;
 };
 
+/*
+DeBug: a way to hold the bot to wait for attaching the Debugger to the process.
+Needs a file DebugWait.txt with one value. 
+When 1 the bot waits, when set to 0 the bot goes on. 
+Set --turnlimit 50000 or something, set to 1, while starting the bot, attach to process, set breakpoints, set to 0.
+*/
+struct DeBug
+{
+	std::ifstream file;
+
+	DeBug()
+	{
+
+	};
+
+	//opens the specified file
+	void open(const std::string &filename)
+	{
+#ifdef _DEBUG
+		file.open(filename.c_str());
+#endif
+	};
+
+	void wait()
+	{
+#ifdef _DEBUG
+		//Timer t;
+		//t.start();
+		//while(true)
+		//{
+		//	bool stopAndRead = ((int)t.getTime() % 2000) < 50;
+		//	open("DebugWait.txt");
+		//	bool bWaitForDebugging = false;
+		//	file >> bWaitForDebugging;
+		//	close();
+		//	if(!bWaitForDebugging)
+		//		break;
+		//}
+#endif
+	}
+
+	//closes the ofstream
+	inline void close()
+	{
+#ifdef _DEBUG
+		file.close();
+#endif
+	};
+};
+
 #endif //BUG_H_
